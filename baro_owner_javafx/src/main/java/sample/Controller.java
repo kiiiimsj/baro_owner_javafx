74a676d1512fded0d55d282a9545b1708764539c
kiiiimsj <kiiiimsj@naver.com>
2020-12-22 20:16:18 +0900

    123

---
+++ baro_owner_javafx/src/main/java/sample/Controller.java
@@ -0,0 +1,129 @@
+package sample;
+
+import com.jfoenix.controls.JFXTabPane;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.scene.Parent;
+import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.text.TextAlignment;
+import java.io.IOException;
+import java.net.URL;
+
+
+public class Controller {
+    @FXML
+    private JFXTabPane tabContainer;
+
+    @FXML
+    private Tab orderListTab;
+    @FXML
+    private AnchorPane orderListContainer;
+
+    @FXML
+    private Tab inventoryManagementTab;
+    @FXML
+    private AnchorPane inventoryManagementContainer;
+
+    @FXML
+    private Tab infoChangeTab;
+    @FXML
+    private AnchorPane infoChangeContainer;
+
+    @FXML
+    private Tab calculateTab;
+    @FXML
+    private AnchorPane calculateContainer;
+
+    @FXML
+    private Tab statisticsTab;
+    @FXML
+    private AnchorPane statisticsContainer;
+
+    @FXML
+    private Tab settingsTab;
+    @FXML
+    private AnchorPane settingsContainer;
+
+
+    private double tabWidth = 90.0;
+    public static int lastSelectedTabIndex = 0;
+    /// Life cycle
+    @FXML
+    public void initialize() {
+        configureView();
+    }
+    /// Private
+    private void configureView() {
+        tabContainer.setTabMinWidth(tabWidth);
+        tabContainer.setTabMaxWidth(tabWidth);
+        tabContainer.setTabMinHeight(tabWidth);
+        tabContainer.setTabMaxHeight(tabWidth);
+        tabContainer.setRotateGraphic(true);
+        EventHandler<Event> replaceBackgroundColorHandler = event -> {
+            lastSelectedTabIndex = tabContainer.getSelectionModel().getSelectedIndex();
+            Tab currentTab = (Tab) event.getTarget();
+            if (currentTab.isSelected()) {
+                currentTab.setStyle("-fx-background-color: -fx-focus-color;");
+            } else {
+                currentTab.setStyle("-fx-background-color: -fx-accent;");
+            }
+        };
+        EventHandler<Event> logoutHandler = event -> {
+            Tab currentTab = (Tab) event.getTarget();
+            if (currentTab.isSelected()) {
+                tabContainer.getSelectionModel().select(lastSelectedTabIndex);
+                //System.out.println("Logging out!");
+            }
+        };
+
+
+
+        configureTab(orderListTab, "주 문", orderListContainer, getClass().getResource("order_list.fxml"), replaceBackgroundColorHandler);
+        configureTab(inventoryManagementTab, "재고관리", inventoryManagementContainer, getClass().getResource("/inventory_management.fxml"), replaceBackgroundColorHandler);
+        configureTab(infoChangeTab, "정보변경", infoChangeContainer, getClass().getResource("/info_change.fxml"), replaceBackgroundColorHandler);
+        configureTab(calculateTab, "정 산", calculateContainer, getClass().getResource("/calculate.fxml"), replaceBackgroundColorHandler);
+        configureTab(statisticsTab, "통 계", statisticsContainer, getClass().getResource("/statistics.fxml"), replaceBackgroundColorHandler);
+        configureTab(settingsTab, "설 정", settingsContainer, getClass().getResource("/settings.fxml"), replaceBackgroundColorHandler);
+
+        orderListTab.setStyle("-fx-background-color: -fx-focus-color;");
+    }
+
+    //이미지경로 넣기 위한 title 뒤에 String iconPath 뺏음
+    private void configureTab(Tab tab, String title, AnchorPane containerPane, URL resourceURL, EventHandler<Event> onSelectionChangedEvent) {
+        double imageWidth = 40.0;
+        //ImageView imageView = new ImageView(new Image(iconPath));
+        //imageView.setFitHeight(imageWidth);
+        //imageView.setFitWidth(imageWidth);
+        Label label = new Label(title);
+        label.setMaxWidth(tabWidth - 20);
+        label.setPadding(new Insets(5, 0, 0, 0));
+        //label.setStyle("-fx-text-fill: black; -fx-font-size: 8pt; -fx-font-weight: normal;");
+        label.setTextAlignment(TextAlignment.CENTER);
+        BorderPane tabPane = new BorderPane();
+        tabPane.setRotate(90.0);
+        tabPane.setMaxWidth(tabWidth);
+        //tabPane.setCenter(imageView);
+        tabPane.setBottom(label);
+        tab.setText("");
+        tab.setGraphic(tabPane);
+        tab.setOnSelectionChanged(onSelectionChangedEvent);
+        if (containerPane != null && resourceURL != null) {
+            try {
+                Parent contentView = FXMLLoader.load(resourceURL);
+                containerPane.getChildren().add(contentView);
+                AnchorPane.setTopAnchor(contentView, 0.0);
+                AnchorPane.setBottomAnchor(contentView, 0.0);
+                AnchorPane.setRightAnchor(contentView, 0.0);
+                AnchorPane.setLeftAnchor(contentView, 0.0);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ baro_owner_javafx/src/main/java/sample/Main.java
@@ -0,0 +1,73 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.drafts.Draft_17;
+import org.java_websocket.handshake.ServerHandshake;
+
+import java.net.URI;
+
+public class Main extends Application {
+
+    //websocket 변수 선언
+    private WebSocketClient webSocketClient;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("/order_list.fxml"));
+        primaryStage.setTitle("Hello World");
+
+        //connect(); //웹소켓 잘 들어옴
+
+        primaryStage.setScene(new Scene(root, 1000, 700));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    //================================================================
+    //websocket
+    private void connect() {
+        System.out.println("aaa");
+        URI uri;
+        try {
+            uri = new URI("ws://3.35.180.57:8080/websocket");
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            return;
+        }
+
+        webSocketClient = new WebSocketClient(uri, new Draft_17()) {
+            @Override
+            public void onOpen(ServerHandshake handshakedata) {
+
+                webSocketClient.send("connect:::" + 1);
+                System.out.println("open!!");
+            }
+
+            @Override
+            public void onMessage(String message) {
+                System.out.println(message);
+            }
+
+            @Override
+            public void onClose(int code, String reason, boolean remote) {
+
+            }
+
+            @Override
+            public void onError(Exception ex) {
+
+            }
+        };
+        webSocketClient.connect();
+    }
+}
